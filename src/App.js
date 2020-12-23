e31106f9b644f44cb23a12dc5ca06a3e4e720c85
loredanamihai <73035324+loredanamihai@users.noreply.github.com>
2020-12-23 17:01:48 +0000

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return  <div className="App"> Weather App</div>    
+  return  (
+    <div className="App"> 
+      <h1>Weather App</h1>
+      <footer>This project is coded by Loredana Mihai and is {" "}
+        <a 
+        href="https://github.com/loredanamihai/react-weather-app"
+        target="_blank"
+        >
+          open-sourced on GitHub
+        </a>        
+      </footer>
+    </div> 
+  );    
+
 }
 
 
