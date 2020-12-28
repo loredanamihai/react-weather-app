efddf2e7971e91085ae2167922e60a67ee777c5d
loredanamihai <73035324+loredanamihai@users.noreply.github.com>
2020-12-28 02:01:22 +0000

    Update URL link

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "89c8092fae86c61da5e71acd50a1415f";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
   }
